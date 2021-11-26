0e9c203c5391121b14bdbb6b07dd93dc6cade815
Uzo <uzo@MacBook-Pro-von-System.local>
2021-11-26 02:02:58 +0100

    removed unused env

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,14 +13,4 @@ export default {
      *  Application port.
      */
     port: process.env.PORT,
-
-    /**
-     * Postgres connection options.
-     */
-    db: {
-        database: process.env.POSTGRES_DB || '',
-        user: process.env.POSTGRES_USER || '',
-        password: process.env.POSTGRES_PASSWORD || '',
-        port: process.env.POSTGRES_PORT || 54320,
-    },
 };
