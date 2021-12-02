d5793b2d3a72761bcbefc7313f44f35b58d2de5d
Uzo <uzo@MacBook-Pro-von-System.local>
2021-12-02 19:08:03 +0100

    testing url in docker

--- locustfile.py
+++ locustfile.py
@@ -2,13 +2,13 @@ from locust import HttpUser, task, TaskSet, between
 
 class PivotCSV(HttpUser):
     wait_time = between(1, 5)
-    
+
     @task(2)
     def pivot_csv_transform(self):
         files = {
             "file": self._get_csv_file("sample.csv"),
         }
-        self.client.post('/pivot-csv/transform', files=files, verify=False)
+        self.client.post('http://localhost:8000/pivot-csv/transform', files=files, verify=False)
 
     def _get_csv_file(self, file_path, file_content_type='text/csv'):
         import os
