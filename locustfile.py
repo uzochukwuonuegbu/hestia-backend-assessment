d680259e3d6f92747cea60548a807800cad16443
Uzo <uzo@MacBook-Pro-von-System.local>
2021-12-02 18:30:35 +0100

    added min and max wait time to locut.py

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,9 @@
 from locust import HttpUser, task, TaskSet, between
 
 class PivotCSV(HttpUser):
-    @task(2)
     wait_time = between(1, 5)
+    
+    @task(2)
     def pivot_csv_transform(self):
         files = {
             "file": self._get_csv_file("sample.csv"),
